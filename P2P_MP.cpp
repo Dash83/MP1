179fa7c5c0d311ec4462ddef6b36a06fb7d12650
Dash83 <caballeromarco@gmail.com>
2016-08-13 15:01:48 -0500

    Initial commit with the filers provided by the instructor.

---
+++ P2P_MP.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : P2P_MP.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
